9ef71198807a77a5612d8378ee26a3903e56b561
Chirag Nanda <chiragnanda@Chirags-MacBook-Air.local>
2025-02-15 18:37:36 +0530

    vc4

--- server/login.js
+++ server/login.js
@@ -1,6 +1,25 @@
 //import {User} from './models/userModel.js';
 import bcrypt from 'bcryptjs';
-import {connectToDatabase} from './dbConnect.js';
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI; // Get the connection string from the environment variable
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db(); // Return the database object
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // Re-throw the error to handle it appropriately
+  }
+}
+
 
 
 export async function login(username, password){
