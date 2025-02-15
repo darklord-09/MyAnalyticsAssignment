79213aedcd5787bdba9b66470aa59152aacee538
Chirag Nanda <chiragnanda@Chirags-MacBook-Air.local>
2025-02-15 17:07:52 +0530

    random

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 export function dbConnect(){
     try{
-        mongoose.connect(`${process.env.DATABASE_URL}`).then(()=>{console.log(result)}).catch((err)=>{console.log(err)});
+        mongoose.connect(`${process.env.DATABASE_URL}`).then(()=>{console.log("mongodb connected")}).catch((err)=>{console.log(err)});
         
         
     }catch(err){
